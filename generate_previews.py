1b6ad78ef7f12eb9f3f55e876b40339054b3bc07
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-09-27 13:12:45 -0400

    Switch to cycles toon shading for preview rendering, display pupils

--- generate_previews.py
+++ generate_previews.py
@@ -9,12 +9,26 @@ import struct
 
 context = bpy.context
 scene = context.scene
+world = bpy.data.worlds[0]
 
 blend_file_directory = bpy.path.abspath("//")
-# TODO: check what happens when you haven't saved the blend file
+if not blend_file_directory:
+  raise Exception("Blend file is not saved. Cannot determine the proper directory.")
+color_masks_dir = os.path.join(blend_file_directory, "color_masks")
+if not os.path.isdir(color_masks_dir):
+  raise Exception("color_masks diretory was not found.")
+preview_dir = os.path.join(blend_file_directory, "preview")
+casual_clothes_tex_path = os.path.join(blend_file_directory, "..", "linktexbci4.png")
+if not os.path.isfile(casual_clothes_tex_path):
+  raise Exception("Casual clothes texture (linktexbci4.png) was not found.")
+bdl_file_path = os.path.join(blend_file_directory, "cl.bdl")
+if not os.path.isfile(bdl_file_path):
+  raise Exception("cl.bdl was not found.")
+tex_headers_path = os.path.join(blend_file_directory, "tex_headers.json")
+if not os.path.isfile(tex_headers_path):
+  raise Exception("tex_headers.json was not found.")
 
 # Need to detect if this model is based on Link, or based on Tetra/Medli, since detecting which mesh is the hat/belt buckle is different for Tetra and Medli compared to Link.
-bdl_file_path = os.path.join(blend_file_directory, "cl.bdl")
 with open(bdl_file_path, "rb") as f:
   f.seek(0x24)
   inf1_section_size = struct.unpack(">I", f.read(4))[0]
@@ -26,13 +40,14 @@ with open(bdl_file_path, "rb") as f:
     belt_buckle_material_name = "m23ear_8_"
 
 # Append the casual hair mesh.
-casual_hair_model_path = os.path.abspath(os.path.join(blend_file_directory, "..", "katsura", "katsura.blend", "Object"))
+casual_hair_model_path = os.path.join(blend_file_directory, "..", "katsura", "katsura.blend", "Object")
 casual_hair_model_mesh_name = "mesh-0"
 bpy.ops.wm.append(
   directory=casual_hair_model_path + "\\", # Needs to have a trailing slash
   filename=casual_hair_model_mesh_name,
 )
 
+# Pose the model so it's not T-posing.
 skeleton_root = scene.objects["skeleton_root"]
 scene.objects.active = skeleton_root
 bpy.ops.object.mode_set(mode="POSE")
@@ -46,6 +61,7 @@ bones["RarmA_jnt"].rotation_euler.rotate_axis("Z", math.radians(-75.0))
 bones["hatA_jnt"].rotation_euler.rotate_axis("Z", math.radians(-50.0))
 bones["hatB_jnt"].rotation_euler.rotate_axis("Z", math.radians(-50.0))
 
+# Pose the casual hair so it's not down by the player's feet.
 casual_hair_skeleton_root = scene.objects["skeleton_root.001"]
 scene.objects.active = casual_hair_skeleton_root
 bones = casual_hair_skeleton_root.pose.bones
@@ -59,7 +75,6 @@ bpy.ops.object.mode_set(mode="OBJECT")
 def update_objects_hidden_in_render(prefix):
   for obj in scene.objects:
     if obj.data.__class__ == bpy.types.Mesh:
-      print(obj.name, obj.data.materials[0].name, hat_material_name, belt_buckle_material_name)
       if obj.data.materials[0].name == hat_material_name:
         # Hide the hat mesh in casual clothes.
         obj.hide_render = (prefix == "casual")
@@ -72,29 +87,33 @@ def update_objects_hidden_in_render(prefix):
       elif obj.data.materials[0].texture_slots[0].texture.image.name == "podAS3TC.png":
         # Hide the sword sheath mesh.
         obj.hide_render = True
+      elif obj.data.materials[0].name in ["m2eyeLdamA", "m3eyeLdamB", "m5eyeRdamA", "m6eyeRdamB"]:
+        # Hide the duplicate eye meshes.
+        obj.hide_render = True
+      elif obj.data.materials[0].name in ["m9mayuLdamA", "m10mayuLdamB", "m12mayuRdamA", "m13mayuRdamB"]:
+        # Hide the duplicate eyebrow meshes.
+        obj.hide_render = True
       else:
         # Show every other mesh.
         obj.hide_render = False
 
 update_objects_hidden_in_render("hero")
 
-bpy.ops.object.lamp_add(
-  type="POINT",
-  location=(5, -120, 90),
-)
-lamp1 = context.object
-lamp1.data.energy = 250
-lamp1.data.distance = 50
-lamp1.data.use_specular = False
-bpy.ops.object.lamp_add(
-  type="POINT",
-  location=(90, 55, 90),
-)
-lamp2 = context.object
-lamp2.data.energy = 250
-lamp2.data.distance = 25
-lamp2.data.use_specular = False
+orig_tex_names_for_objs = {}
+for obj in scene.objects:
+  if obj.data.__class__ == bpy.types.Mesh:
+    orig_tex_names_for_objs[obj] = obj.data.materials[0].texture_slots[0].texture.image.name
 
+scene.render.resolution_x = 900
+scene.render.resolution_y = 1300
+
+# Make the background transparent (for the masks).
+scene.render.layers["RenderLayer"].use_sky = False
+
+# Masks need to be binary, they won't work properly if they have antialiasing.
+scene.render.use_antialiasing = False
+
+# Add camera.
 bpy.ops.object.camera_add(
   location=(90, -120, 90),
   rotation=(math.radians(80), math.radians(0), math.radians(36.5)),
@@ -108,14 +127,7 @@ camera = context.object
 scene.camera = camera
 camera.data.clip_end = 5000.0
 
-scene.render.resolution_x = 900
-scene.render.resolution_y = 1300
-scene.render.layers["RenderLayer"].use_sky = False
-scene.render.use_antialiasing = False
-
-for item in bpy.data.materials:
-  item.use_shadeless = False
-
+# Make materials use transparency correctly (for the masks).
 for material in bpy.data.materials:
   material.use_transparency = True
   material.alpha = 0.0
@@ -124,36 +136,21 @@ for material in bpy.data.materials:
       continue
     texture_slot.use_map_alpha = True
 
-# Change the texture wrapping mode of all textures based on tex_headers.json so they render correctly.
-tex_headers_path = os.path.join(blend_file_directory, "tex_headers.json")
+# Change the texture wrapping mode of all textures based on tex_headers.json so they render correctly (for the masks).
 with open(tex_headers_path) as f:
   tex_headers = json.load(f)
+tex_wrap_mode_for_image_name = {}
 for tex_header in tex_headers:
   tex_image_name = tex_header["Name"] + ".png"
   if tex_header["WrapS"] == "ClampToEdge":
     wrap_mode = "EXTEND"
   else:
     wrap_mode = "REPEAT"
+  tex_wrap_mode_for_image_name[tex_image_name] = wrap_mode
   for tex in bpy.data.textures:
     if isinstance(tex, bpy.types.ImageTexture):
       if tex.image.name == tex_image_name:
         tex.extension = wrap_mode
-
-# Since Blender can't display pupils in the eyes, we use the closed eyes texture so it doesn't look crepy.
-bpy.data.images["eyeh.1.png"].filepath = os.path.join(blend_file_directory, "eyeh.4.png")
-
-# Render the hero clothes preview.
-preview_dir = os.path.join(blend_file_directory, "preview")
-if not os.path.exists(preview_dir):
-  os.mkdir(preview_dir)
-scene.render.filepath = os.path.join(preview_dir, "preview_hero.png")
-bpy.ops.render.render(write_still=True)
-
-# Render the casual clothes preview.
-bpy.data.images["linktexS3TC.png"].filepath = os.path.join(blend_file_directory, "..", "linktexbci4.png")
-update_objects_hidden_in_render("casual")
-scene.render.filepath = os.path.join(preview_dir, "preview_casual.png")
-bpy.ops.render.render(write_still=True)
 
 # Now generate the masks by replacing the model's textures with the texture masks and disabling shading.
 for item in bpy.data.materials:
@@ -165,7 +162,14 @@ for texture in bpy.data.textures:
   texture.filter_type = "FELINE" # This filter seems to be the only one that can be made to have absolutely no antialiasing.
   texture.filter_size = 0.1
 
-color_masks_dir = os.path.join(blend_file_directory, "color_masks")
+# Make a copy of all images so we can change those into the masks.
+for image in bpy.data.images:
+  mask_image = bpy.data.images.load(image.filepath)
+  mask_image.name = image.name + "_mask"
+  for tex in bpy.data.textures:
+    if isinstance(tex, bpy.types.ImageTexture):
+      if tex.image == image:
+        tex.image = mask_image
 
 for prefix in ["hero", "casual"]:
   color_mask_file_paths = glob.glob(os.path.join(color_masks_dir, "%s_*.png" % prefix))
@@ -189,7 +193,7 @@ for prefix in ["hero", "casual"]:
     
     # Change the textures to be completely red or white, but also preserve the original alpha channel.
     for texture_name in textures_to_mask:
-      image = bpy.data.images[texture_name]
+      image = bpy.data.images[texture_name + "_mask"]
       pixels = image.pixels[:] # Convert the image's pixels to a tuple to increase performance when reading from it
       new_pixels = [] # Instead of modifying the image's pixels every loop, make a new list of pixels we edit for better performance
       for i in range(len(image.pixels)//4):
@@ -197,7 +201,7 @@ for prefix in ["hero", "casual"]:
         new_pixels += [1.0, 0.0, 0.0, orig_alpha]
       image.pixels[:] = new_pixels # Now update the image's actual pixels just once with our pixel list
     for texture_name in textures_to_not_mask:
-      image = bpy.data.images[texture_name]
+      image = bpy.data.images[texture_name + "_mask"]
       pixels = image.pixels[:]
       new_pixels = []
       for i in range(len(image.pixels)//4):
@@ -205,6 +209,116 @@ for prefix in ["hero", "casual"]:
         new_pixels += [1.0, 1.0, 1.0, orig_alpha]
       image.pixels[:] = new_pixels
     
-    bpy.data.images["linktexS3TC.png"].filepath = os.path.join(color_masks_dir, "%s_%s.png" % (prefix, curr_color_name))
+    bpy.data.images["linktexS3TC.png_mask"].filepath = os.path.join(color_masks_dir, "%s_%s.png" % (prefix, curr_color_name))
     scene.render.filepath = os.path.join(preview_dir, "preview_%s_%s.png" % (prefix, curr_color_name))
     bpy.ops.render.render(write_still=True)
+
+
+
+
+# Now render the actual preview images, using Cycles so we can have proper toon shading.
+scene.render.engine = "CYCLES"
+world.light_settings.use_ambient_occlusion = True
+world.light_settings.ao_factor = 0.3
+scene.cycles.film_transparent = True
+
+# Add lighting.
+bpy.ops.object.lamp_add(
+  type="SUN",
+  location=(17, -82, 100),
+  rotation=(math.radians(75), math.radians(15), math.radians(0)),
+)
+lamp1 = context.object
+lamp1.data.shadow_soft_size = 0.04
+lamp1.data.use_specular = False
+
+# Create the pupil image.
+bpy.data.images.load(os.path.join(blend_file_directory, "hitomi.png"))
+
+# Create the Cycles materials for all objects.
+done_mat_names = []
+image_nodes = []
+for obj in scene.objects:
+  if obj.data.__class__ == bpy.types.Mesh:
+    mat = obj.data.materials[0]
+    tex_name = orig_tex_names_for_objs[obj]
+    
+    if mat.name in done_mat_names:
+      continue
+    done_mat_names.append(mat.name)
+    
+    mat.use_shadeless = False
+    mat.use_nodes = True
+    
+    nodes = mat.node_tree.nodes
+    links = mat.node_tree.links
+    
+    bsdf_node = None
+    output_node = None
+    for node in mat.node_tree.nodes:
+      if node.__class__ == bpy.types.ShaderNodeBsdfDiffuse:
+        bsdf_node = node
+        break
+      elif node.__class__ == bpy.types.ShaderNodeOutputMaterial:
+        output_node = node
+        break
+    if bsdf_node:
+      nodes.remove(bsdf_node)
+    
+    image_node = nodes.new("ShaderNodeTexImage")
+    image_node.image = bpy.data.images[tex_name]
+    image_nodes.append(image_node)
+    
+    toon_node = nodes.new("ShaderNodeBsdfToon")
+    
+    if tex_name == "mayuh.1.png":
+      # Eyebrows. Need transparent backgrounds.
+      mix_node = nodes.new("ShaderNodeMixShader")
+      transparent_node = nodes.new("ShaderNodeBsdfTransparent")
+      
+      link = links.new(image_node.outputs[0], toon_node.inputs[0])
+      link = links.new(image_node.outputs[1], mix_node.inputs[0])
+      link = links.new(transparent_node.outputs[0], mix_node.inputs[1])
+      link = links.new(toon_node.outputs[0], mix_node.inputs[2])
+      link = links.new(mix_node.outputs[0], output_node.inputs[0])
+    elif tex_name == "eyeh.1.png":
+      # Eyes. Need transparent backgrounds and for the pupil to be overlayed on the whites of the eyes.
+      mix_node = nodes.new("ShaderNodeMixShader")
+      transparent_node = nodes.new("ShaderNodeBsdfTransparent")
+      mixrgb_node = nodes.new("ShaderNodeMixRGB")
+      mixrgb_node.blend_type = "MULTIPLY"
+      pupil_image_node = nodes.new("ShaderNodeTexImage")
+      pupil_image_node.image = bpy.data.images["hitomi.png"]
+      image_nodes.append(pupil_image_node)
+      
+      link = links.new(image_node.outputs[0], mixrgb_node.inputs[1])
+      link = links.new(pupil_image_node.outputs[0], mixrgb_node.inputs[2])
+      link = links.new(pupil_image_node.outputs[1], mixrgb_node.inputs[0])
+      link = links.new(mixrgb_node.outputs[0], toon_node.inputs[0])
+      link = links.new(image_node.outputs[1], mix_node.inputs[0])
+      link = links.new(transparent_node.outputs[0], mix_node.inputs[1])
+      link = links.new(toon_node.outputs[0], mix_node.inputs[2])
+      link = links.new(mix_node.outputs[0], output_node.inputs[0])
+    else:
+      # Everything else. Just a simple toon shader.
+      link = links.new(image_node.outputs[0], toon_node.inputs[0])
+      link = links.new(toon_node.outputs[0], output_node.inputs[0])
+
+# Change the texture wrapping mode again, but this time for cycles.
+for tex_image_name, wrap_mode in tex_wrap_mode_for_image_name.items():
+  for image_node in image_nodes:
+    if image_node.image.name == tex_image_name:
+      image_node.extension = wrap_mode
+
+# Render the hero clothes preview.
+if not os.path.exists(preview_dir):
+  os.mkdir(preview_dir)
+update_objects_hidden_in_render("hero")
+scene.render.filepath = os.path.join(preview_dir, "preview_hero.png")
+bpy.ops.render.render(write_still=True)
+
+# Render the casual clothes preview.
+bpy.data.images["linktexS3TC.png"].filepath = casual_clothes_tex_path
+update_objects_hidden_in_render("casual")
+scene.render.filepath = os.path.join(preview_dir, "preview_casual.png")
+bpy.ops.render.render(write_still=True)
