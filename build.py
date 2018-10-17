388452b3912c5eea8831571a426a9b3896fc36ff
LagoLunatic <LagoLunatic@users.noreply.github.com>
2018-10-16 20:12:33 -0400

    Add preview generator stuff to release builds

--- build.py
+++ build.py
@@ -7,3 +7,5 @@ with ZipFile("./dist/WW_Model_Helpers.zip", "w") as zip:
   zip.write("fix_normals.py", arcname="fix_normals.py")
   zip.write("make_materials_shadeless.py", arcname="make_materials_shadeless.py")
   zip.write("README.md", arcname="README.txt")
+  zip.write("generate_previews.py", arcname="generate_previews.py")
+  zip.write("generate_previews_README.md", arcname="generate_previews_README.md")
