4ff7e49f79b9dda2e453ce23a378a841e0c42453
LagoLunatic <LagoLunatic@users.noreply.github.com>
2020-06-21 18:08:48 -0400

    Update build script for python 3.8

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 
-py -3.6-32 -m PyInstaller unpack_models.spec
-py -3.6-32 -m PyInstaller pack_models.spec
-py -3.6-32 build.py
+py -3.8-32 -m PyInstaller unpack_models.spec
+py -3.8-32 -m PyInstaller pack_models.spec
+py -3.8-32 build.py
 
-py -3.6-64 -m PyInstaller unpack_models.spec
-py -3.6-64 -m PyInstaller pack_models.spec
-py -3.6-64 build.py
+py -3.8-64 -m PyInstaller unpack_models.spec
+py -3.8-64 -m PyInstaller pack_models.spec
+py -3.8-64 build.py
