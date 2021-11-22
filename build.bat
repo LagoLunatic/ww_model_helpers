c4c38331a8b9937eef1ab088b8d3fd145ba2fdd2
LagoLunatic <LagoLunatic@users.noreply.github.com>
2021-11-22 13:29:03 -0500

    Update Python to 3.9.6

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 
-py -3.8-32 -m PyInstaller unpack_models.spec
-py -3.8-32 -m PyInstaller pack_models.spec
-py -3.8-32 build.py
+py -3.9-32 -m PyInstaller unpack_models.spec
+py -3.9-32 -m PyInstaller pack_models.spec
+py -3.9-32 build.py
 
-py -3.8-64 -m PyInstaller unpack_models.spec
-py -3.8-64 -m PyInstaller pack_models.spec
-py -3.8-64 build.py
+py -3.9-64 -m PyInstaller unpack_models.spec
+py -3.9-64 -m PyInstaller pack_models.spec
+py -3.9-64 build.py
